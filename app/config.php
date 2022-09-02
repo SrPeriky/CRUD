844b1d5810d73daed5519d6557302753be80146b
GermanVelandia <german.velandia@swissaxis.ch>
2022-09-02 15:00:05 -0500

    Agregar el readme

--- app/config.php
+++ app/config.php
@@ -6,7 +6,7 @@ define('PSSWORD', '');
 define('_HOY_', date('Y-m-d'));
 define('DEFAULT_CONTROLLER', 'user');
 define('DEFAULT_ACTION', 'index');
-define('BASE_URL', 'http://localhost/crud/');
+define('BASE_URL', 'http://localhost/CRUD/');
 define('MSG_ERROR_DB', 'Se produjo un error al conectar con la base de datos');
 define('MSG_ERROR_404', 'Se produjo un error, no se ah encontrado la ruta');
 date_default_timezone_set('America/Bogota');
